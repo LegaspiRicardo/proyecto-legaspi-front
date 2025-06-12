3c0a80cfe6f5d1abcda3b689ad6ad2b7cfcc71df
Legaspi Ricardo <108900203+LegaspiRicardo@users.noreply.github.com>
2025-06-11 19:16:24 -0600

    Registro de usuarios desde el Front

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextField, Button, Container, Box, Typography, Link} from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Register: React.FC = () => {
     const navigate = useNavigate();
-    const Nnavigate = useNavigate();
+    //Estado para manejar los datos del formulario
+    const [username, setUsername] = useState('');
+    const [email , setEmail ] = useState ('');
+    const [password , setPassword ] = useState ('');
+    const [errorMessage , setErrorMessage ] = useState ('');
 
-    const handleRegister = (event: React.FormEvent) => {
-        event.preventDefault();
-        //Aqui ira la logica para registrar al usuario
-        console.log("Usuario registrado");
-        navigate("/login");  //Redirigir a la pagina de inicio despues del registro
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();   //Evitar recargar de pagina al enviar el formulario
+        try{
+            //Enviar los datos al backend
+            const response = await axios.post('http://localhost:5000/api/users/register' , {
+                username,
+                email,
+                password
+            });
+            console.log("Registro exitosillo: ", response.data);
+            navigate("/Dashboard");  //Redirigir a la pagina de inicio despues de iniciar sesion
+        }catch (error:any){
+            console.error(error);
+            setErrorMessage(error.response?.data?.message || "Error al registrar el usuario");
+        }
 
     };
 
@@ -21,13 +37,15 @@ const Register: React.FC = () => {
                     Registro
                 </Typography>
 
-                <Box component={"form"} noValidate sx={{mt:2}} onSubmit={handleRegister}>
+                <Box component={"form"} noValidate sx={{mt:2}} onSubmit={handleSubmit}>
 
                     <TextField
                         fullWidth
                         label="Nombre"
                         margin="normal"
                         type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                         required
                     />
 
@@ -36,6 +54,8 @@ const Register: React.FC = () => {
                         label="Correo Electronico"
                         margin="normal"
                         type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
 
@@ -44,15 +64,25 @@ const Register: React.FC = () => {
                         label="Contraseña"
                         margin="normal"
                         type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
+
+                    {errorMessage && (
+                        <Typography color="error" variant="body2" sx={{mt: 1}} >
+                          {errorMessage}  
+                        </Typography>
+                    )}
+
 
                     <Button
                         fullWidth
                         variant="contained"
                         color="primary"
                         sx={{mt:3}}
-                    >Ingresar</Button>
+                        type="submit"
+                    >Registrar</Button>
 
                 </Box>
             </Box>
@@ -62,7 +92,7 @@ const Register: React.FC = () => {
                 ¿Ya tienes cuenta? {' '}
                 <Link component="button"
                 variant="body2"
-                onClick={()=> Nnavigate('/')} >
+                onClick={()=> navigate('/')} >
                 Inicia sesión aqui</Link>
                 
 
